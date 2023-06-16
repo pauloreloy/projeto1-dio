504b4f66e36def7ccf6ab1241874d4cb90b9b719
Paulo Rogerio Eloy <pauloreloy@gmail.com>
2023-06-16 00:32:19 -0400

    FC

---
+++ backend/conexao.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "database-svc";
+$username = "root";
+$password = "Projeto1_Paulo";
+$database = "meubanco";
+
+// Criar conexão
+
+
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+?>
---
+++ database/meubanco.sql
@@ -0,0 +1,6 @@
+CREATE TABLE mensagens(
+    id int(100),
+    nome varchar(100),
+    email varchar(100),
+    comentario varchar(100)
+)
